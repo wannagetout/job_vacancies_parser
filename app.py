fbb8f4f4b4d8856547f2c93b10dc6fd40779c0cd
wannagetout <inv.moiseev@gmail.com>
2023-04-13 07:34:49 +0300

    Correct SJ parsing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from instances.base.connector import Connector
 from instances.hh.service import HH
+from instances.sj.service import SuperJob
 from utils import utils
 
 
@@ -7,7 +8,9 @@ if __name__ == '__main__':
     keyword = input("Введите ключевую фразу для поиска:")
     file = Connector(filename=f'{keyword}.json')
     hh = HH(keyword)
+    sj = SuperJob(keyword)
     file.insert(data=hh.get_json(), datafile=file.data_file)
+    file.insert(data=sj.get_json(), datafile=file.data_file)
     print(
         f"Загружено 1000 вакансий в файл {keyword}.json:\n"
         "Нажмите 1 для просмотра топ 10 вакансий по зарплате\n"
@@ -20,12 +23,14 @@ if __name__ == '__main__':
         if user_input == 1:
             top_10 = utils.get_top(keyword)
             for item in top_10:
+                print('----------------------------------')
                 print(
                     f'Вакансия: {item["vacancy_name"]}\n'
                     f'Зарплата: {item["salary"]}\n'
                     f'Описание: {item["snippet"]}\n'
                     f'Ссылка: {item["url"]}\n'
                 )
+                print('----------------------------------')
         if user_input == 2:
             request_field = input("Введите поле для поиска:")
             request_value = input("Введите информацию для поиска:")
--- instances/hh/service.py
+++ instances/hh/service.py
@@ -1,5 +1,4 @@
 from instances.base.engine_classes import Engine
-from instances.hh.vacancy import HHVacancy
 from requests import request
 
 from instances.vacancies.job_classes import Vacancy
@@ -63,42 +62,3 @@ class HH(Engine):
 			vacancy_dict['salary'] = vacancy.salary
 			result.append(vacancy_dict)
 		return result
-
-	# def get_entities_list(self) -> list[HHVacancy]:
-	# 	vacancies = self.get_json_from_request()
-	# 	vacancies_list = []
-	# 	for vacancy in vacancies:
-	# 		if vacancy['salary'] is None:
-	# 			continue
-	# 		vacancy_entity = HHVacancy(vacancy)
-	# 		vacancies_list.append(vacancy_entity)
-	# 	return vacancies_list
-
-#
-# hh = HH()
-# vacanciess = hh.get_json()
-# for v in vacanciess:
-# 	print(v)
-
-# vacancy_id = 0
-# vacancies_mapping = []
-# for i in vacancies:
-# 	vacancy = Vacancy(data=i)
-# 	vacancies_mapping.append(vacancy)
-# 	print(i)
-# 	print(i['id'])
-# 	print(i['name'])
-# 	print(i['alternate_url'])
-# 	print(i.get('salary')['from'] and i.get('salary')['to'] if i.get('salary') else None)
-# 	print(i.get('snippet')['requirement'])
-# 	print('==============================')
-#
-# for i in vacancies_mapping:
-# 	print(i)
-# t = hh.get_request(params={'area': 113, 'page': 1, 'per_page': 20, 'text': 'Python'})
-
-# for a in t.json()['items']:
-# 	print(a['name'])
-# 	print(a['salary'])
-# 	print(a['snippet']['requirement'])
-# 	print('======================')
